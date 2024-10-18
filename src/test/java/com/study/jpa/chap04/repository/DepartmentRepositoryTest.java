0bb84bae636026075bb901c58eac564cd343d2ee
bildno <gingu33@naver.com>
2024-10-18 11:28:28 +0900

    양방향 관계에서 데이터 갱신이 반대편에 반영되지 않는 문제 해결

--- src/test/java/com/study/jpa/chap04/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/study/jpa/chap04/repository/DepartmentRepositoryTest.java
@@ -2,6 +2,7 @@ package com.study.jpa.chap04.repository;
 
 import com.study.jpa.chap04.entity.Department;
 import com.study.jpa.chap04.entity.Employee;
+import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,9 @@ class DepartmentRepositoryTest {
     @Autowired
     DepartmentRepository departmentRepository;
 
+    @Autowired
+    EntityManager em;
+
     @Test
     @DisplayName("부서 정보 조회하면 해당 부서원들도 함꼐 조회되어야한다")
     void findDept() {
@@ -42,7 +46,7 @@ class DepartmentRepositoryTest {
 
     @Test
     @DisplayName(".")
-    void testMethod() {
+    void testLazyAndEager() {
         // 3번 사원을 조회할거임 부서정보는 필요 없음
         // given
         Long id = 3L;
@@ -52,6 +56,32 @@ class DepartmentRepositoryTest {
         System.out.println("\n\n\n\n\n");
         System.out.println("employee = " + employee);
         System.out.println("\n\n\n\n\n");
+    }
+    
+    
+    @Test
+    @DisplayName("양 방향 연관관계에서 연관 데이터의 수정")
+    void testChangeDept() {
+        // 1번 사원의 부서를 1-> 2번 부서로 변경해야댐
+        // given
+        Employee foundEmp = employeeRepository.findById(1L).orElseThrow();
+        Department newDept = departmentRepository.findById(2L).orElseThrow();
+
+        employeeRepository.save(foundEmp);
+
+
+
+        /*
+        em.flush(); // DB로 밀어내기
+        em.clear(); // 영속성 컨텍스트 비우기(비우지 않으면 켄텍스트 내의 정보를 참조하려고 함)
+        */
+        // when
+        // 연관관계 편의 메서드 호출 -> 데이터 수정 시 반대편 엔터티도 꼭 수정을 해주쟈
+        foundEmp.changeDepartment(newDept);
+        // then
+        System.out.println("\n\n\n\n");
+        newDept.getEmployees().forEach(System.out::println);
+        System.out.println("\n\n\n\n");
 
     }
 
